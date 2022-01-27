c45d5be1575a41a2069f260f1563cefada8b3075
Ilya Firsov <firsov1996@yandex.ru>
2022-01-27 19:11:25 +0100

    27-01-2022 Meal List  finished

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import '../widgets/meal_item.dart';
+
+import '/dummy_data.dart';
 
 class CategoryMealScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -17,12 +20,24 @@ class CategoryMealScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'] as String;
     final categoryid = routeArgs['id'] as String;
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(categoryid);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Container(
-        child: const Text('The Recipes For The Category!'),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          return MealItem(
+            title: categoryMeals[index].title,
+            imageUrl: categoryMeals[index].imageUrl,
+            duration: categoryMeals[index].duration,
+            complexity: categoryMeals[index].complexity,
+            affordability: categoryMeals[index].affordability,
+          );
+        },
+        itemCount: categoryMeals.length,
       ),
     );
   }
